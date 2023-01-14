40c45dbd18d587e12a7642961ded5325a9ddef57
Kauan Ribeiro Teruel <114168887+KauanRTe@users.noreply.github.com>
2023-01-14 17:12:13 -0300

    Exercício 4 em Csharp

---
+++ ExercicioCSharp4/Exercicio4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            // Vetor e Variáveis
+            int[] vetor = {5, 20, 4, 7, 9, 3};
+            int somatoriaVetor = 0;
+            int mediaVetor = 0;
+
+            // Métodos
+            void somatoria(){
+                for(int i=0; i < vetor.Length; i++){
+                    somatoriaVetor = somatoriaVetor + vetor[i];
+                }
+            }
+
+            somatoria();
+
+            void media(){
+                mediaVetor = somatoriaVetor / vetor.Length;
+            }
+            
+            media();
+
+            //Função Principal
+            Console.WriteLine("A somatória do vetor é: " + somatoriaVetor);
+            Console.WriteLine("A média do vetor é: " + mediaVetor);
+            Console.ReadKey();
+           
+
+        }
+    }
+}
