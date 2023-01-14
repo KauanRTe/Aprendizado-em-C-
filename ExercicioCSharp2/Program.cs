bedd6af2b0534323f2952417156a1585b08e497d
Kauan Ribeiro Teruel <114168887+KauanRTe@users.noreply.github.com>
2023-01-14 17:11:03 -0300

    Exercício 2 em CSharp

---
+++ ExercicioCSharp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args){
+            // Variáveis
+            int numMultiplicar, numMultiplicado, resultado = 0;
+
+            // Método da soma
+            void multiplicar(){
+                for(int i=1; i <= numMultiplicar; i++){
+                    resultado = resultado + numMultiplicado;
+                }
+            }
+
+            // Main Program
+
+            Console.WriteLine("Qual o número a ser multiplicado? ");
+            numMultiplicado = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Por quanto será multiplicado? ");
+            numMultiplicar = int.Parse(Console.ReadLine());
+
+            multiplicar();
+
+            Console.WriteLine("O resultado da multiplicação é: " + resultado);
+
+        }
+    }
+}
