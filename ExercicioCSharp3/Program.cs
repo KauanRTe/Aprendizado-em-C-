d7a549a07fd1996be2297ed67c7b94f79f99e9df
Kauan Ribeiro Teruel <114168887+KauanRTe@users.noreply.github.com>
2023-01-14 17:11:32 -0300

    Exercício 3 em CSharp

---
+++ ExercicioCSharp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio5{
+
+    public class Equacao
+    {
+        public int numero = 0;
+        public int potencia = 0;
+        public int resultado = 0;
+
+        public void EquacaoExponencial(){
+            Console.WriteLine("Qual o número a ser potencializado?");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a potência?");
+            potencia = int.Parse(Console.ReadLine());
+
+            resultado = numero;
+
+            for(int i=1; i < potencia; i++){
+                resultado = resultado * numero;
+            }
+
+        }
+
+        }
+       class Program
+        {
+        static void Main(string[] args){
+
+            // Main Program
+            Equacao equacao = new Equacao();
+
+            equacao.EquacaoExponencial();
+
+            Console.WriteLine("O resultado da equação é: " + equacao.resultado);
+
+        }
+    }
+}
